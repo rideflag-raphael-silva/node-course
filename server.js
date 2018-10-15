5d3c85ed4f67a8214256e20042e17bb0b546dc9f
Raphael Ferras <raphaelferras@gmail.com>
2018-10-14 21:18:10 -0400

    Setup start script and heroky port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -63,5 +64,6 @@ app.get('/bad',(req, res) => {
   });
 }
 );
-
-app.listen(3000,() => console.log('Server is up and running'));
+/**/
+//app.listen(3000,() => console.log('Server is up and running'));
+app.listen(port,() => console.log(`Server is up and running ${port}`));
